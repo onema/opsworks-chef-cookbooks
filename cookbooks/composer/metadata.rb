481af213b10b5bc8683c76aec7e832ea4285a638
Juan Manuel Torres <juan.torres@alleluu.com>
2013-03-25 12:34:11 -0700

    adding a bunch of test files

---
+++ cookbooks/composer/metadata.rb
@@ -0,0 +1,14 @@
+maintainer       "Escape Studios"
+maintainer_email "dev@escapestudios.com"
+license          "MIT"
+description      "Installs/Configures Composer"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.2"
+
+supports "ubuntu"
+supports "debian"
+supports "centos"
+supports "redhat"
+supports "fedora"
+
+depends "php"
