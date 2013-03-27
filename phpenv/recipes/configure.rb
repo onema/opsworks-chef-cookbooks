f5f4da4dd1a15ae8a281ef042200cb7c93654bb9
Juan Manuel Torres <juan.torres@alleluu.com>
2013-03-27 14:52:57 -0700

    updated template to add spacing and EOL characters. also should print the application name.

--- phpenv/recipes/configure.rb
+++ phpenv/recipes/configure.rb
@@ -5,8 +5,9 @@ node[:deploy].each do |application, deploy|
     owner deploy[:user] 
     group deploy[:group]
     mode "0660"
+    puts 'THIS IS THE APPLICATION NAME YYYYYYYYY: ' + application
 
-    variables( :env => node[:custom_env])
+    variables( :env => node[:custom_env][application])
 
     only_if do
      File.directory?("#{deploy[:deploy_to]}/current/public")
