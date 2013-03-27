fffec6e16f00b7373b588a7f43b0dd63207e5234
Onema <kinojman@gmail.com>
2013-03-27 12:12:57 -0700

    updated recipe to use deploy user as the owner of the file

--- phpenv/recipes/configure.rb
+++ phpenv/recipes/configure.rb
@@ -2,14 +2,9 @@ node[:deploy].each do |application, deploy|
   
   template "#{deploy[:deploy_to]}/current/public/.htaccess" do
     source "htaccess.erb"
+    owner deploy[:user] 
     group deploy[:group]
     mode "0660"
-
-    if platform?("ubuntu")
-      owner "www-data"
-    elsif platform?("amazon")
-      owner "apache"
-    end
 
     variables( :env => node[:custom_env][application])
 
