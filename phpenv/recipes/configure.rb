5bc0ffcd14bc87b9e595f57f77fc23a255b4c21c
Onema <kinojman@gmail.com>
2013-04-26 11:30:39 -0700

    updated values to use application name to set values.

--- phpenv/recipes/configure.rb
+++ phpenv/recipes/configure.rb
@@ -6,10 +6,7 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     mode "0660"
 
-    puts ">>> #{application}" 
-    puts ">>> #{node[:deploy][:application]}" 
-
-    variables( :env => node[:custom_env], :application1 => ">>> #{application}", :application2 =>  ">>> #{node[:deploy][:application]}" )
+    variables( :env => node[:custom_env], :application => "#{application}" )
 
     only_if do
      File.directory?("#{deploy[:deploy_to]}/current/public")
