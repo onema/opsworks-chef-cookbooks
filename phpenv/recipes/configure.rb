452c36e853dfcb61fc7faf9600e04b66e749d4ea
Onema <kinojman@gmail.com>
2013-04-27 10:55:57 -0700

    update to select values based on application name

--- phpenv/recipes/configure.rb
+++ phpenv/recipes/configure.rb
@@ -6,7 +6,11 @@ node[:deploy].each do |application, deploy|
     group deploy[:group]
     mode "0660"
 
-    variables( :env => node[:custom_env]["#{application}"], :application => "#{application}" )
+    node[:custom_env].each do |key, value|
+        if key.to_s == application.to_s
+            variables( :values => value[:values], :application => "#{application}" )
+        end
+    end
 
     only_if do
      File.directory?("#{deploy[:deploy_to]}/current/public")
