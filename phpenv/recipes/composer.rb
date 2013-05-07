a9bb47b3ca37bb3e1ee4e2e1641d86112c2addf6
onema <kinojman@gmail.com>
2013-05-07 12:35:02 -0700

    added comment

--- phpenv/recipes/composer.rb
+++ phpenv/recipes/composer.rb
@@ -1,3 +1,7 @@
+#
+# Taken from:
+# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
+#
 node[:deploy].each do |application, deploy|
   script "install_composer" do
     interpreter "bash"
