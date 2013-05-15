94930464da72ed0c8b3e1bdb85ee925d3e669205
onema <kinojman@gmail.com>
2013-05-14 17:06:11 -0700

    [#49626557] added independent configuration values for database, the generated file must be included.

---
+++ phpenv/recipes/dbconfig.rb
@@ -0,0 +1,26 @@
+node[:deploy].each do |app_name, deploy|
+
+  template "#{deploy[:deploy_to]}/current/db-connect.php" do
+    source "db-connect.php.erb"
+    mode 0660
+    group deploy[:group]
+
+    if platform?("ubuntu")
+      owner "www-data"
+    elsif platform?("amazon")
+      owner "apache"
+    end
+
+    variables(
+      :host => (deploy[:database][:host] rescue nil),
+      :user => (deploy[:database][:username] rescue nil),
+      :password => (deploy[:database][:password] rescue nil),
+      :db => (deploy[:database][:database] rescue nil),
+      :table => (node[:phpapp][:dbtable] rescue nil)
+    )
+
+   only_if do
+     File.directory?("#{deploy[:deploy_to]}/current")
+   end
+  end
+end
