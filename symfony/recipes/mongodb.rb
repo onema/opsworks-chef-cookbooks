8633230b28a319330ab0311d958e06fd9bcea36c
onema <kinojman@gmail.com>
2013-05-21 23:23:17 -0700

    updated mongodb recipe to use opsworks pear os package to install mongodb php driver

--- symfony/recipes/mongodb.rb
+++ symfony/recipes/mongodb.rb
@@ -1,4 +1,15 @@
 # install the mongodb pecl
-php_pear "mongo" do
-  action :install
-end
+#php_pear "mongo" do
+#  action :install
+#end
+
+node[:deploy].each do |application, deploy|
+  script "install_php_mongodb" do
+    interpreter "bash"
+    user "root"
+    cwd "/"
+    code <<-EOH
+    pecl install mongo
+    EOH
+  end
+end 
