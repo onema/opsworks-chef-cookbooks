ac7f794b13c1cf2a3da2536ec1339b6b1e26a3a3
onema <kinojman@gmail.com>
2013-05-21 22:39:31 -0700

    created symfony specific recipes

---
+++ symfony/recipes/configure.rb
@@ -0,0 +1,15 @@
+node[:deploy].each do |application, deploy|
+  
+  template "#{deploy[:deploy_to]}/current/web/.htaccess" do
+    source "htaccess.erb"
+    owner deploy[:user] 
+    group deploy[:group]
+    mode "0660"
+
+    variables( :env => node[:custom_env], :application => "#{application}" )
+
+    only_if do
+     File.directory?("#{deploy[:deploy_to]}/current/web")
+    end
+  end
+end
