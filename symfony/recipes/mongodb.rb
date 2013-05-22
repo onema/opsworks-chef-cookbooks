8656c11d316e9d80eca14b1f3020d17cb70f0cfc
onema <kinojman@gmail.com>
2013-05-22 07:40:21 -0700

    updated mongo recipe to add mongo.ini in the correct php configuration path.

--- symfony/recipes/mongodb.rb
+++ symfony/recipes/mongodb.rb
@@ -13,3 +13,27 @@ node[:deploy].each do |application, deploy|
     EOH
   end
 end 
+
+#execute 'install_php_mongo_driver' do
+#  if node[:kernel][:machine] == 'x86_64'
+#    libdir = 'lib64'
+#  else
+#    libdir = 'lib'
+#  end
+#  command "pecl install mongo"
+#  action :run
+#end
+
+template 'mongo.ini' do
+  case node[:platform]
+  when 'centos','redhat','fedora','amazon'
+    path "/etc/php.d/mongo.ini"
+  when 'debian','ubuntu'
+    path "/etc/php5/conf.d/mongo.ini"
+  end
+  source 'mongo.ini.erb'
+  owner 'root'
+  group 'root'
+  mode 0644
+  notifies :restart, resources(:service => 'apache2')
+end
