ffb9803ecc2a3cb7306c26a1bf614c45ae640664
onema <kinojman@gmail.com>
2013-05-24 22:53:31 -0700

    [#50556603] updated missing comas.

--- symfony/recipes/paramconfig.rb
+++ symfony/recipes/paramconfig.rb
@@ -16,8 +16,8 @@ node[:deploy].each do |application, deploy|
       :user => (deploy[:database][:username] rescue nil),
       :password => (deploy[:database][:password] rescue nil),
       :db => (deploy[:database][:database] rescue nil),
-      :table => (node[:phpapp][:dbtable] rescue nil)
-      :mailer_user => (node[:custom_env][application.to_s][:mailer_user] rescue nil)
+      :table => (node[:phpapp][:dbtable] rescue nil),
+      :mailer_user => (node[:custom_env][application.to_s][:mailer_user] rescue nil),
       :mailer_password => (node[:custom_env][application.to_s][:mailer_password] rescue nil)
     )
 
