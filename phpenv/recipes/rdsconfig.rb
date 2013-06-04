37fab777626169ec6b96cbca13ae27e24dfbed8a
onema <kinojman@gmail.com>
2013-06-03 23:58:21 -0700

    recipe update

--- phpenv/recipes/rdsconfig.rb
+++ phpenv/recipes/rdsconfig.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy|
 
-  template "#{deploy[:deploy_to]}/current/fuel/app/config/#{node[application.to_s][:environment]}/db.php" do
+  template "#{deploy[:deploy_to]}/current/fuel/app/config/#{node[:custom_env][application.to_s][:environment]}/db.php" do
     source "db.php.erb"
     mode 0660
     group deploy[:group]
@@ -12,11 +12,11 @@ node[:deploy].each do |application, deploy|
     end
 
     variables(
-      :dbname => (node[application.to_s][:database][:dbname] rescue nil),
-      :host => (node[application.to_s][:database][:host] rescue nil),
-      :user => (node[application.to_s][:database][:username] rescue nil),
-      :password => (node[application.to_s][:database][:password] rescue nil),
-      :port => (node[application.to_s][:database][:port] rescue nil)
+      :dbname => (node[:custom_env][application.to_s][:database][:dbname] rescue nil),
+      :host => (node[:custom_env][application.to_s][:database][:host] rescue nil),
+      :user => (node[:custom_env][application.to_s][:database][:username] rescue nil),
+      :password => (node[:custom_env][application.to_s][:database][:password] rescue nil),
+      :port => (node[:custom_env][application.to_s][:database][:port] rescue nil)
     )
 
    only_if do
