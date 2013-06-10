c287a011775e62eefb1ffd8b7748755607f3d0a3
onema <kinojman@gmail.com>
2013-06-10 13:23:55 -0700

    change working directory to '/'

--- phpenv/recipes/memoryswap.rb
+++ phpenv/recipes/memoryswap.rb
@@ -7,7 +7,7 @@
 script "memory_swap" do
   interpreter "bash"
   user "root"
-  cwd "#{deploy[:deploy_to]}"
+  cwd "/"
   code <<-EOH
   /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=2048
   /sbin/mkswap /var/swap.1
