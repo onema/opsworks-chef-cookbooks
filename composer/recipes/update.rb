78e593a98451c22125b7f8b1987d2f54941cdc8e
onema <kinojman@gmail.com>
2013-06-13 14:52:15 -0700

    moved recipes around to be grouped in more intuitive cookbooks.

---
+++ composer/recipes/update.rb
@@ -0,0 +1,15 @@
+#
+# Taken from:
+# http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
+#
+node[:deploy].each do |application, deploy|
+  script "install_composer" do
+    interpreter "bash"
+    user "root"
+    cwd "#{deploy[:deploy_to]}/current"
+    code <<-EOH
+    curl -s https://getcomposer.org/installer | php
+    php composer.phar update
+    EOH
+  end
+end 
