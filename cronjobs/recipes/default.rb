8ac13c8ae345905c4beb35e64f7114afdc4db1cf
Juan Manuel Torres <kinojman@gmail.com>
2013-10-31 16:03:32 -0700

    updates to cronjob recipe

--- cronjobs/recipes/default.rb
+++ cronjobs/recipes/default.rb
@@ -1,18 +1,18 @@
 node[:deploy].each do |application, deploy|
 
   node[:custom_env][application.to_s][:cron_jobs].each do |cron_values|
-    value_minute = cron_values[:minute]   rescue '*'
-    value_hour   = cron_values[:hour]     rescue '*'
-    value_day    = cron_values[:day]      rescue '*'
-    value_month  = cron_values[:month]    rescue '*'
-    value_weekday= cron_values[:weekday]  rescue '*'
+#    value_minute = cron_values[:minute]   rescue '*'
+#    value_hour   = cron_values[:hour]     rescue '*'
+#    value_day    = cron_values[:day]      rescue '*'
+#    value_month  = cron_values[:month]    rescue '*'
+#    value_weekday= cron_values[:weekday]  rescue '*'
 
     cron "#{cron_values[:name]}" do
-      minute  "#{value_minute.to_s}"
-      hour    "#{value_hour.to_s}"
-      day     "#{value_day.to_s}"
-      month   "#{value_month.to_s}"
-      weekday "#{value_weekday.to_s}"
+      minute  "#{cron_values[:minute]}"
+      hour    "#{cron_values[:hour]}"
+      day     "#{cron_values[:day]}"
+      month   "#{cron_values[:month]}"
+      weekday "#{cron_values[:weekday]}"
       command "#{cron_values[:command]}"
     end
   end
