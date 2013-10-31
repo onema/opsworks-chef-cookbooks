b39a38f49fd23d057e0da37ae03c90b3e1c4e03f
Juan Manuel Torres <kinojman@gmail.com>
2013-10-31 13:47:50 -0700

    updates to cronjob recipe

--- cronjobs/recipes/default.rb
+++ cronjobs/recipes/default.rb
@@ -1,19 +1,19 @@
 node[:deploy].each do |application, deploy|
 
   node[:custom_env][application.to_s][:cron_jobs].each do |cron_values|
-    minute = cron_values[:minute]   rescue '*'
-    hour   = cron_values[:hour]     rescue '*'
-    day    = cron_values[:day]      rescue '*'
-    month  = cron_values[:month]    rescue '*'
-    weekday= cron_values[:weekday]  rescue '*'
+    value_minute = cron_values[:minute]   rescue '*'
+    value_hour   = cron_values[:hour]     rescue '*'
+    value_day    = cron_values[:day]      rescue '*'
+    value_month  = cron_values[:month]    rescue '*'
+    value_weekday= cron_values[:weekday]  rescue '*'
 
     cron "#{cron_values[:name]}" do
-      minute  "#{minute}"
-      hour    "#{hour}"
-      day     "#{day}"
-      month   "#{month}"
-      weekday "#{weekday}"
-      command "#{command}"
+      minute  value_minute.to_s
+      hour    value_hour.to_s
+      day     value_day.to_s
+      month   value_month.to_s
+      weekday value_weekday.to_s
+      command value_command.to_s
     end
   end
 end
