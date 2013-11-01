0efc0ead12d75bc0bd3efceab7fe49e8e5bd5311
Juan Manuel Torres <kinojman@gmail.com>
2013-10-31 18:44:59 -0700

    Cleanup

--- cronjobs/recipes/default.rb
+++ cronjobs/recipes/default.rb
@@ -1,12 +1,6 @@
 node[:deploy].each do |application, deploy|
 
   node[:custom_env][application.to_s][:cron_jobs].each do |cron_values|
-#    value_minute = cron_values[:minute]   rescue '*'
-#    value_hour   = cron_values[:hour]     rescue '*'
-#    value_day    = cron_values[:day]      rescue '*'
-#    value_month  = cron_values[:month]    rescue '*'
-#    value_weekday= cron_values[:weekday]  rescue '*'
-
     cron "#{cron_values[:name]}" do
       minute  "#{cron_values[:minute]}"
       hour    "#{cron_values[:hour]}"
