28adf85305fef06c7e2ffa83c5eb012432a3cfb8
Juan Manuel Torres <kinojman@gmail.com>
2014-07-07 14:19:10 -0700

    Update code to enable modules

--- phpenv/recipes/php_mongodb.rb
+++ phpenv/recipes/php_mongodb.rb
@@ -1,14 +1,14 @@
 # Use pecl to install module
-execute 'install_php_mongo_module' do
+execute "install_php_mongo_module" do
   command "pecl install mongo"
   action :run
 end
 
-template 'mongo.ini' do
+template "mongo.ini" do
   case node[:platform]
-  when 'centos','redhat','fedora','amazon'
+  when "centos","redhat","fedora","amazon"
     path "/etc/php.d/mongo.ini"
-  when 'debian','ubuntu'
+  when "debian","ubuntu"
     path "/etc/php5/conf.d/mongo.ini"
         only_if { ::File.exist?("/etc/php5/conf.d")}
 
@@ -22,12 +22,12 @@ template 'mongo.ini' do
   variables(
     :module_name => "mongo"
   )
-  notifies :restart, resources(:service => 'apache2')
+  notifies :restart, resources(:service => "apache2")
 end
 
 case node[:platform]
-when 'debian','ubuntu'
-  user 'root'
+when "debian","ubuntu"
+  user "root"
   execute "enable_redis" do
     command "php5enmod mongo && service apache restart"
     only_if { ::File.exist?("/etc/php5/mods-available")}
--- phpenv/recipes/php_redis.rb
+++ phpenv/recipes/php_redis.rb
@@ -1,33 +1,33 @@
 # Use pecl to install module
-execute 'install_php_redis_module' do
+execute "install_php_redis_module" do
   command "pecl install redis"
   action :run
 end
 
-template 'redis.ini' do
+template "redis.ini" do
   case node[:platform]
-  when 'centos','redhat','fedora','amazon'
+  when "centos","redhat","fedora","amazon"
     path "/etc/php.d/redis.ini"
-  when 'debian','ubuntu'
+  when "debian","ubuntu"
     path "/etc/php5/conf.d/redis.ini"
         only_if { ::File.exist?("/etc/php5/conf.d")}
 
     path "/etc/php5/mods-available/redis.ini"
         only_if { ::File.exist?("/etc/php5/mods-available")}
   end
-  source 'php_module.ini.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
+  source "php_module.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
   variables(
     :module_name => "redis"
   )
-  notifies :restart, resources(:service => 'apache2')
+  notifies :restart, resources(:service => "apache2")
 end
 
 case node[:platform]
-when 'debian','ubuntu'
-  user 'root'
+when "debian","ubuntu"
+  user "root"
   execute "enable_redis" do
     command "php5enmod reids && service apache restart"
     only_if { ::File.exist?("/etc/php5/mods-available")}
