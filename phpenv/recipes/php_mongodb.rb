050394389bc7a8573f8ebc4d6a56bc9715145974
Juan Manuel Torres <kinojman@gmail.com>
2014-07-07 11:57:33 -0700

    adding generic template and update code to set template in the correct location

--- phpenv/recipes/php_mongodb.rb
+++ phpenv/recipes/php_mongodb.rb
@@ -1,23 +1,49 @@
-execute 'install_php_mongo_driver' do
+# Use pecl to install module
+execute 'install_php_mongo_module' do
   command "pecl install mongo"
   action :run
 end
 
-template 'mongo.ini' do
-  case node[:platform]
+# Find out where to place the ini file
+case node[:platform]
   when 'centos','redhat','fedora','amazon'
-    path "/etc/php.d/mongo.ini"
+    path_to_ini = "/etc/php.d"
+
   when 'debian','ubuntu'
-    path "/etc/php5/conf.d/mongo.ini"
+    path_to_ini "/etc/php5/conf.d"
         not_if { ::File.exist?("/etc/php5/conf.d")}
 
-    path "/etc/php5/mods-available/mongo.ini"
+    path_to_ini "/etc/php5/mods-available"
         not_if { ::File.exist?("/etc/php5/mods-available")}
   end
-  source 'mongo.ini.erb'
-  owner 'root'
-  group 'root'
-  mode 0644
+end
+
+# Use template resource to set the ini file in the correct location
+template "#{path_to_ini}/mongo.ini" do
+  source "php_module.ini.erb"
+  owner root
+  group root
+  mode "0644"
+  variables(
+    :module_name => "mongo"
+  )
+  only_if do
+    File.directory?("#{path_to_ini}")
+  end
   notifies :restart, resources(:service => 'apache2')
 end
 
+# enable module and restart apache
+case node[:platform]
+when 'debian','ubuntu'
+  script "enable_php_mongo_driver" do
+    interpreter "bash"
+    user "root"
+
+    code <<-EOH
+      php5enmod mongo
+      service apache restart
+    EOH
+    not_if { ::File.exist?("#{path_to_ini}")}
+  end
+end
