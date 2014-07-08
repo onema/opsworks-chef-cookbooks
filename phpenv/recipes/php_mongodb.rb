e9c65b2ff160104f2291749b22ef0b9b12b9454b
Juan Manuel Torres <kinojman@gmail.com>
2014-07-07 17:29:28 -0700

    Update code to enable modules.

--- phpenv/recipes/php_mongodb.rb
+++ phpenv/recipes/php_mongodb.rb
@@ -1,3 +1,40 @@
-# Install a pecl module
-::Chef::Resource.send(:include, Phpenv::Helper)
-Phpenv::Helper.pecl_install("mongo")
+# Use pecl to install module
+module_name = "mongo"
+
+execute "install_php_#{module_name}_module" do
+  command "pecl install #{module_name}"
+  action :run
+end
+
+# Create template
+template "#{module_name}.ini" do
+  case node[:platform]
+  when "centos","redhat","fedora","amazon"
+    path "/etc/php.d/#{module_name}.ini"
+  when "debian","ubuntu"
+    path "/etc/php5/conf.d/#{module_name}.ini"
+        only_if { ::File.exist?("/etc/php5/conf.d")}
+
+    path "/etc/php5/mods-available/#{module_name}.ini"
+        only_if { ::File.exist?("/etc/php5/mods-available")}
+  end
+  source "php_module.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+    :module_name => "#{module_name}"
+  )
+  notifies :restart, resources(:service => "apache2")
+end
+
+
+# Place enable the module if required and restart apache
+case node[:platform]
+when "debian","ubuntu"
+  user "root"
+  execute "enable_#{module_name}" do
+    command "php5enmod #{module_name} && service apache restart"
+    only_if { ::File.exist?("/etc/php5/mods-available/#{module_name}")}
+  end
+end
