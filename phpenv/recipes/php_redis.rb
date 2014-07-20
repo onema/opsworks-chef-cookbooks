39a22c9888b0d22639bfe900c81ff991277edd56
Juan Manuel Torres <kinojman@gmail.com>
2014-07-20 01:03:07 -0700

    Updating redis recipe to correctly install on different platform versions

--- phpenv/recipes/php_redis.rb
+++ phpenv/recipes/php_redis.rb
@@ -9,24 +9,27 @@ execute "install_php_#{module_name}_module" do
   action :run
 end
 
+
 # Create template
 template "#{module_name}.ini" do
-  case node[:platform]
-  when "centos","redhat","fedora","amazon"
-    path "/etc/php.d/#{module_name}.ini"
-  when "debian","ubuntu"
-    path "/etc/php5/conf.d/#{module_name}.ini"
-        only_if { ::File.exist?("/etc/php5/conf.d")}
-
-    path "/etc/php5/mods-available/#{module_name}.ini"
-        only_if { ::File.exist?("/etc/php5/mods-available")}
-  end
   source "php_module.ini.erb"
+  case node[:platform]
+    when "centos","redhat","fedora","amazon"
+      path "/etc/php.d/#{module_name}.ini"
+    when "debian","ubuntu"
+
+      if node[:platform_version].to_f >= 14.04
+        path "/etc/php5/mods-available/#{module_name}.ini"
+      else
+        path "/etc/php5/conf.d/#{module_name}.ini"
+      end
+
+  end
   owner "root"
   group "root"
   mode "0644"
   variables(
-    :module_name => "#{module_name}"
+    :name => "#{module_name}"
   )
   notifies :restart, resources(:service => "apache2")
 end
@@ -37,7 +40,8 @@ case node[:platform]
 when "debian","ubuntu"
   execute "enable_#{module_name}" do
     user "root"
-    command "php5enmod #{module_name} && service apache2 restart"
+    command "php5enmod #{module_name}"
     only_if { ::File.exist?("/etc/php5/mods-available/#{module_name}.ini")}
+    notifies :restart, resources(:service => "apache2")
   end
 end
