d2d7fa7f4a49bbde15260d2fac88c5cf2aea6755
Christopher Roussel <christopher@impleri.net>
2015-03-23 15:40:53 -0500

    Use attributes from deploy

--- composer/recipes/install.rb
+++ composer/recipes/install.rb
@@ -3,10 +3,9 @@
 # http://docs.aws.amazon.com/opsworks/latest/userguide/gettingstarted.walkthrough.photoapp.3.html
 #
 node[:deploy].each do |application, deploy|
-  script "install_composer" do
-    only_if { application[:application_type] == "php" }
-    interpreter "bash"
-    user "root"
+  bash "install_composer" do
+    only_if { deploy[:application_type] == "php" }
+    user deploy[:user]
     cwd "#{deploy[:deploy_to]}/current"
     code <<-EOH
     curl -s https://getcomposer.org/installer | php
