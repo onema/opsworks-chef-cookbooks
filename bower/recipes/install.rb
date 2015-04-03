2e795f23443acb361fb585edacce39e52d17ba79
Alton Torregano <alton.torregano@c4tech.com>
2015-04-03 14:11:31 -0500

    Added brunch recipe for build

--- bower/recipes/install.rb
+++ bower/recipes/install.rb
@@ -4,7 +4,9 @@
 #
 node[:deploy].each do |application, deploy|
    execute "bower_install" do
+
         command "bower install --allow-root"
         cwd "#{deploy[:deploy_to]}/current"
+        only_if do :: File::exists?('./bower.json') end
    end
 end
