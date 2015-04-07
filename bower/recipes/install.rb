fca0853fc7534a817fbcb00639b8333c9cf8ca98
Alton Torregano <alton.torregano@c4tech.com>
2015-04-07 11:58:16 -0500

    --production

--- bower/recipes/install.rb
+++ bower/recipes/install.rb
@@ -5,7 +5,7 @@
 node[:deploy].each do |application, deploy|
    execute "bower_install" do
         user deploy[:user]
-        command "bower install"
+        command "bower install --production"
         cwd "#{deploy[:deploy_to]}/current"
         only_if {::File.exists?('./bower.json') }
    end
