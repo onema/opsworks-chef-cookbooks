132baa2c5a1c6bdc004f63c4a35de758d2d65c22
Juan Manuel Torres <kinojman@gmail.com>
2015-06-13 12:25:24 -0700

    Deleting cookbook, this cookbook is deprecated and is being deleted for good.

--- fuel/metadata.rb
+++
@@ -1,7 +0,0 @@
-name              "fuel"
-maintainer        "Juan Manuel Torres"
-maintainer_email  "kinojman@gmail.com"
-license           "MIT"
-description       "NOT MAINTAINED - set of recipes to deploy a FuelPHP app."
-version           "0.1.0"
-
