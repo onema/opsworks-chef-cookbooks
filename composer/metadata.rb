243b02831efa6bb347bd7a2cc6922f67951372be
Juan Manuel Torres <kinojman@gmail.com>
2015-06-13 12:03:53 -0700

    Deleting cookbook, this has been moved to https://github.com/onema/opsworks-composer

--- composer/metadata.rb
+++
@@ -1,7 +0,0 @@
-name              "composer"
-maintainer        "Juan Manuel Torres"
-maintainer_email  "kinojman@gmail.com"
-license           "MIT"
-description       "Installs or Updates PHP Project dependencies using Composer"
-version           "1.0.0"
-
